89d6847afa0f5f45870725a1431143382c01082b
Your Name <you@example.com>
2024-03-31 05:26:24 +0500

    First Commit

---
+++ Alerts/app.js
@@ -0,0 +1,79 @@
+// Answer # 1
+
+// alerts
+
+// Answer # 2
+
+// ;
+
+// Answer # 3
+
+// ()
+
+// Answer # 4
+
+// statement
+
+// Answer # 5
+
+// alert("Hi there, Sailor!");
+
+// Answer # 6
+
+// alert("Thanks for everything!");
+
+// Answer # 7
+
+// alert("Boo!");
+
+// Answer # 8
+
+// alert
+
+// Answer # 9
+
+// ");
+
+// Answer # 10
+
+// alert("Hi");
+
+// Answer # 11
+
+// ("");
+
+// Answer # 12
+
+// alert("Hello World");
+
+// Answer # 13
+
+// alert
+
+// Answer # 14
+
+// ");
+
+// Answer # 15
+
+// alert("Hi");
+
+// Answer # 16
+
+// ("");
+
+// Answer # 17
+
+// alert("Hello World");
+
+// Answer # 18
+
+// alert("Naveed Ahmed");
+
+// Answer # 19
+
+// alert("Hello World");
+
+// Answer # 20
+
+// alert("Naveed Ahmed");
